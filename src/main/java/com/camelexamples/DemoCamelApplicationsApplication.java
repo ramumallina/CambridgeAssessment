0c19c5889155d95c0bb6fa8ab66a6105d72c9374
unknown <ramumallina9999@gmail.com>
2023-02-04 13:21:33 +0530

    Initial Commit

---
+++ src/main/java/com/camelexamples/DemoCamelApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.camelexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCamelApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCamelApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camelexamples/DemoCamelApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camelexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCamelApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
